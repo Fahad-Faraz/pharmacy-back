f422707ee91d7b947251dfc600c3f0d89dc27531
fahad-faraz <fahadfaraz9ok@gmail.com>
2026-06-19 13:23:36 -0700

    fix errorHandler import

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import express from "express";
 import cors from "cors";
 
 import connectDB from "./config/db.js";
-import errorHandler from "./middleware/error.js";
+import {errorHandler} from "./middleware/error.js";
 
 // routes
 import authRoutes from "./routes/authRoutes.js";
--- middleware/error.js
+++ middleware/error.js
@@ -2,3 +2,4 @@ export const errorHandler = (err, req, res, next) => {
     const status = err.status || 500;
     res.status(status).json({ message: err.message || "Internal server error" });
 };
+export default errorHandler;
